fdf1daa9ee44f437fde4c6bdc249af56abb7a698
NderituMwanu <mwanugerald@gmail.com>
2020-04-11 16:51:39 +0300

    fixed a bug limiting the functioning of the http service

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -3,22 +3,33 @@ import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/map';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProfileService {
 
-  private username:string;
-  private clientid:'860b7118b7d0d48d66c1';
-  private clientSecret:'6abeedb8a2254184c3469d3a64a782522ab611f8';
-  
-  constructor(private http: HttpClient) { 
+  private username: any;
+  private clientid: '860b7118b7d0d48d66c1';
+  private clientSecret: 'b5fc39835d729e8df8796d747d780e071108a611';
+
+  constructor(private http: HttpClient) {
     console.log("our service is running");
     this.username = "Nderitumwanu";
   }
 
-    getProfInfo(){
-      return this.http.get("https://api.github.com/users/NderituMwanu" + "?client_id=" + this.clientid + "&client_secret="
+
+  getProfileInfo() {
+    return this.http.get("https://api.github.com/users/"+ this.username + "?client_id=" + this.clientid + "&client_secret="
       + this.clientSecret)
-      .map(res => res); 
-    }
+      .map(res => res);
+  }
+
+  getProfileRepos() {
+    return this.http.get("https://api.github.com/users/"+ this.username +  "/repos?client_id=" + this.clientid + "&client_secret="
+      + this.clientSecret)
+      .map(res => res);
+  }
+
+  updateProfile(username: string) {
+    this.username = username;
+  }
 }
